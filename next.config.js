01211952624c0267fcdf341e5c60576c11ac2dae
viniciusdes <viniciussilva9200@gmail.com>
2023-04-20 09:50:29 -0300

    feat: merge first pages

--- next.config.js
+++ next.config.js
@@ -1,26 +1,7 @@
-const { NextFederationPlugin } = require("@module-federation/nextjs-mf");
 /** @type {import('next').NextConfig} */
 
 const nextConfig = {
   reactStrictMode: true,
-  // webpack: (config, options) => {
-  //   const { isServer } = options;
-
-  //   config.experiments = { topLevelAwait: true };
-  //   config.plugins.push(
-  //     new NextFederationPlugin({
-  //       name: "manufacturing",
-  //       remotes: {
-  //         "auth-saibweb-front": `auth-saibweb-front@http://localhost:3022/_next/static/${
-  //           isServer ? "ssr" : "chunks"
-  //         }/remoteEntry.js`,
-  //       },
-  //       filename: "static/chunks/primaryEntry.js",
-  //     })
-  //   );
-
-  //   return config;
-  // },
   compiler: {
     styledComponents: true,
   },
--- src/layout/logged/index.tsx
+++ src/layout/logged/index.tsx
@@ -3,32 +3,18 @@ import GlobalStyle from "@/styles/global";
 import React from "react";
 import { Content, ContentArea, Wrapper } from "./styles";
 import WindowSizeListener from "react-window-size-listener";
-import { useAuth } from "@/store/authSlice";
 
-import { useRouter } from "next/navigation";
 import dynamic from "next/dynamic";
 
 import { ToastContainer } from "react-toastify";
 import Header from "@/components/header";
 
-// const Header = dynamic(() => import("auth-saibweb-front/label"));
-
 function LoggedLayout({ children }: { children: React.ReactNode }) {
   const [Altura, setAltura] = React.useState(0);
-
-  const authStore = useAuth();
-
-  if (!authStore.authData.signed) {
-    // setTimeout(() => {
-    // router.replace("/");
-    // }, 200);
-    // return <div />;
-  }
 
   return (
     <main>
       <GlobalStyle />
-      {/* <Header /> */}
 
       <Wrapper translate="no">
         <WindowSizeListener
