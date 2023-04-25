70d6b7e9c436dfe91f0db0062f4208c44ee08c2a
Vinicius <viniciuscruz@saibweb.com.br>
2023-04-25 15:27:54 -0300

    remove bug image

--- src/components/menu/styles.ts
+++ src/components/menu/styles.ts
@@ -194,7 +194,7 @@ export const SideMenuTopContainer = styled.div`
 
 export const SideMenuLogoContainer = styled.img`
   height: 156px;
-  /* The image used */
+
   width: 100%;
   background-color: #f3f3f3;
 
